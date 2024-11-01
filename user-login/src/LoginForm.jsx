e65f33890b315186a0b1140b0ade4a0d4e5081d9
Isaac Micheal <Mike600Isaac@gmail.com>
2024-11-01 15:30:08 +0100

    My useState login

--- user-login/src/LoginForm.jsx
+++ user-login/src/LoginForm.jsx
@@ -1,11 +1,26 @@
+import {useState} from "react";
+
+
 
 
 function LoginForm() {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    // function handleChange(evt) {
+    //     let newName = evt.target.value;
+    //     return setUsername(newName);
+    // }
+    // function handleChange2(evt) {
+    //     let newPassword = evt.target.value;
+    //     return setPassword(newPassword);
+    // }
+
     return (
         <div>
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Password"/>
-            <button>Login</button>
+            <input type="text" placeholder="Username" value={username} onChange={ (evt) => { return setUsername(evt.target.value) }}/>
+            <input type="text" placeholder="Password" value={password} onChange={ (evt) => { return setPassword(evt.target.value) }}/>
+            <button onClick={() => {return alert((`Welcome ${username} with password ${password}`))}}>Login</button>
         </div>
     );
 }
